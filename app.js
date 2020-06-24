78207cd5a6a553cbbc27944c9f64e6372bb7c614
Spartacus <k_morter@outlook.com>
2020-06-24 10:47:07 -0600

    finished site feautures

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+const generatePage = require('./src/page-template.js');
 // const profileDataArgs = process.argv.slice(2);
 // const [name, github] = profileDataArgs;
 
@@ -128,10 +128,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw new Error(err);
-        console.log('Page created! Check out index.html in this directory to see it!');
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
-  });
